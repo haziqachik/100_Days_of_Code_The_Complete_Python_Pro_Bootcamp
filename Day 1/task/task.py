0248e5ba9e706b94df8f7add6435182169b87435
haziq <haziqh@hotmail.com>
2024-09-08 23:27:03 +0800

    Commit

---
+++ Day 1/task/task.py
@@ -0,0 +1,5 @@
+print("Welcome to the band name generator website yoooo")
+city = input("which city were you born in bruh?\n")
+pet = input("what is ur pet just a random qn?\n")
+
+print ("your sick band name is " + city + " " + pet)
